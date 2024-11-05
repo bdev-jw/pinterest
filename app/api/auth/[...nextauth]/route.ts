a99524d2c14e759583c78cc19b26fab2b452a2e5
bedv-jw <0523wns@naver.com>
2024-11-05 15:06:56 +0900

    update route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -32,4 +32,5 @@ export const authOptions: NextAuthOptions = {
 
 const handler = NextAuth(authOptions)
 
+// route handlers만 export
 export { handler as GET, handler as POST }
